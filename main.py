e2bce303d4013fe9b5dec327975669d90b512d51
Ilya Bannov <bannov@reg.ru>
2021-07-06 15:45:03 +0400

    Добавизапись логов в файл searchlog, сам файл добавил в gitignore

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, escape
 from vsearch import search4letters
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return "Hi"
+
+
 
 @app.route('/search4' ,methods=['POST'])
 def do_search() -> str:
     pharse = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(pharse, letters))
+    results = str(search4letters(pharse, letters))
+    log_request(request, results)
+    return results
 
+@app.route('/')
 @app.route('/entry' ,methods=['POST'])
 def entry_page():
     return render_template('entry.html',
     the_title="Welcome search4letters")
 
+@app.route('/logs')
+def view_log() -> str:
+    with open('searchlog') as log:
+        contents = log.read()
+    return escape(contents)
 
-app.run(debug=True)
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('searchlog', 'a') as log:
+        print(req.form,  file=log, end='|')
+        print(req.remote_addr,  file=log, end='|')
+        print(req.user_agent,  file=log, end='|')
+        print(res,  file=log)
+
+if __name__ == '__main__':
+    app.run(debug=True)
