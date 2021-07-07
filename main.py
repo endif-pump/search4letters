43bac7ee588f447cd51f888e2b4a7e596144586e
Ilya Bannov <bannov@reg.ru>
2021-07-08 00:02:34 +0400

    Добавил вызов диспетчера контекста в view_log

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, escape
+from flask import Flask, render_template, request, redirect, escape, session
 from vsearch import search4letters
 from DBcm import UseDatabase
 import pymysql
@@ -7,17 +7,13 @@ dbconfig = {
     'host': '172.17.0.2',
     'user': 'root',
     'password': 'my-secret-pw',
-    'database': 'searchlogDB',
-} 
-
-
+    'database': 'searchlog',
+}
 
 app = Flask(__name__)
 
 
-
-
-@app.route('/search4' ,methods=['POST'])
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
     pharse = request.form['phrase']
     letters = request.form['letters']
@@ -25,17 +21,25 @@ def do_search() -> str:
     log_request(request, results)
     return results
 
+
 @app.route('/')
-@app.route('/entry' ,methods=['POST'])
+@app.route('/entry', methods=['POST'])
 def entry_page():
-    return render_template('entry.html',
-    the_title="Welcome search4letters")
+    return render_template('entry.html', the_title="Welcome search4letters")
+
 
 @app.route('/logs')
 def view_log() -> str:
-    with open('searchlog') as log:
-        contents = log.read()
+    contents = ''
+    with UseDatabase(dbconfig) as cursor:
+
+        _SQL = """SELECT phrase, letters, ip, browser_string, results FROM log"""
+        cursor.execute(_SQL )
+        contents = cursor.fetchall()
+        print(contents)
     return escape(contents)
+        
+
 
 def log_request(req: 'flask_request', res: str) -> None:
     with UseDatabase(dbconfig) as cursor:
@@ -44,12 +48,14 @@ def log_request(req: 'flask_request', res: str) -> None:
                   (phrase, letters, ip, browser_string, results)
                   values
                   (%s, %s, %s, %s, %s)"""
-    
-        cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res, ))
+
+        cursor.execute(_SQL, (
+            req.form['phrase'],
+            req.form['letters'],
+            req.remote_addr,
+            req.user_agent.browser,
+            res,
+        ))
 
 
 if __name__ == '__main__':
